057d1736e616f2674c85067a9501c0ce7afbf91a
Boussami Nassim <nassim.boussami@machinestalk.com>
2023-10-06 11:14:11 +0100

    make location dynamic

--- server.js
+++ server.js
@@ -8,11 +8,16 @@ const wss = new WebSocket.Server({ port: 8082 });
 
 wss.on('connection', ws => {
     console.log('client connected');
+    let lat = 36.847305
+    let lng = 10.213722
     setInterval(() => {
+      lat += lat < 37.220507 ? 0.003165 : -0.003165
+      lng += lng < 10.090782 ? 0.003165 : -0.003165
       const location = {
-        latitude: 10.021145,
-        longitude: 32.265474,
+        lat: lat.toFixed(6),
+        lng: lng.toFixed(6),
       };
+      console.log(location)
       ws.send(JSON.stringify(location));
     }, 1000);
   }
